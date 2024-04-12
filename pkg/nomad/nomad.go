b6b829dfcb436ebf11ddcbaa7e2a2136debcaea7
Brian Cain <bcain@hashicorp.com>
2024-04-12 10:52:03 -0700

    Add some funcs to the Nomad client

--- pkg/nomad/nomad.go
+++ pkg/nomad/nomad.go
@@ -1,6 +1,10 @@
 package nomad
 
-import "github.com/hashicorp/nomad/api"
+import (
+	"context"
+
+	"github.com/hashicorp/nomad/api"
+)
 
 type Nomad struct {
 	// Nomad client
@@ -8,5 +12,48 @@ type Nomad struct {
 }
 
 func NewNomad() (*Nomad, error) {
-	return &Nomad{}, nil
+	client, err := api.NewClient(api.DefaultConfig())
+	if err != nil {
+		return nil, err
+	}
+
+	return &Nomad{
+		client: client,
+	}, nil
+}
+
+func (n *Nomad) Create(
+	ctx context.Context,
+	job *api.Job,
+) (*api.JobRegisterResponse, error) {
+	resp, _, err := n.client.Jobs().Register(job, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return resp, nil
+}
+
+func (n *Nomad) Delete(
+	ctx context.Context,
+	jobID string,
+) error {
+	_, _, err := n.client.Jobs().Deregister(jobID, true, nil)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (n *Nomad) Status(
+	ctx context.Context,
+	jobID string,
+) (*api.Job, error) {
+	job, _, err := n.client.Jobs().Info(jobID, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return job, nil
 }
